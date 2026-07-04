f78640e0eccdf8f453e412b06f8fe5253aa75089
Daniel Wilches <dwilches@proton.me>
2026-07-04 16:28:12 -0400

    Fix base url for GHPages

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,13 @@
 import type { Config } from "@react-router/dev/config";
 
+const isGHPagesBuild = process.env.NODE_ENV === "production";
+
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
+
+  // Allows serving the SPA from GH Pages
+  basename: isGHPagesBuild ? "/KazeMatsuri/" : "/",
+
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
+const isGHPagesBuild = process.env.NODE_ENV === "production";
+
 export default defineConfig({
+  // Allows serving the SPA from GH Pages
+  base: isGHPagesBuild ? "/KazeMatsuri/" : "/",
+
   plugins: [tailwindcss(), reactRouter()],
+
   resolve: {
     tsconfigPaths: true,
   },
